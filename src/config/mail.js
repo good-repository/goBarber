5efe16ff7b887cd61fda8aef56796b8d38e55e7b
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-04-29 14:35:27 -0300

    nodemailer configuration

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '768c48c2ad7490',
+        pass: '107d195714f0cf'
+    },
+    default: {
+        from: 'Jaguatirica <titira@bla.com'
+    }
+}
