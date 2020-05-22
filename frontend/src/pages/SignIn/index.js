a7780f9ebf2bedc2c7fa8c174b15d3e89ca44594
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-05-22 11:19:58 -0300

    Login and Register pages stylized

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg'
 
 function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt='GoBarber' />
+
+      <form>
+        <input type='email' placeholder='Digite seu email' />
+        <input type='password' placeholder='Digite sua senha' />
+
+        <button type='submit'>Acessar</button>
+        <Link to='/register'>Crie sua conta</Link>
+      </form>
+    </>
+  )
 }
 
 export default SignIn;
--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg'
 
 function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <img src={logo} alt='GoBarber' />
+
+      <form>
+        <input placeholder='Digite seu nome completo' />
+        <input type='email' placeholder='Digite seu email' />
+        <input type='password' placeholder='Digite sua senha' />
+
+        <button type='submit'>Criar conta</button>
+        <Link to='/'>Já tenho uma conta</Link>
+      </form>
+    </>
+  )
 }
 
 export default SignUp;
