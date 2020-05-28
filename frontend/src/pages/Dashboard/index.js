0f9516faefdcc7229398866acc4f90bde572cbc8
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-05-28 10:41:10 -0300

    stylized Dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import api from '~/services/api'
 
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
-
-
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 function Dashboard() {
-  api.get('appointments')
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type='button'>
+          <MdChevronLeft size={36} color='#FFF' />
+        </button>
+        <strong>data qualquer</strong>
+        <button type='button'>
+          <MdChevronRight size={36} color='#FFF' />
+        </button>
+      </header>
+
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Guilherme Borelli</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>disponível</span>
+        </Time>
+        <Time >
+          <strong>10:00</strong>
+          <span>Guilherme Borelli</span>
+        </Time>
+      </ul>
+    </Container>
+  )
 }
 
 export default Dashboard;
