b01e199993fa586d25bc60039da8241baef63ade
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-06-01 14:05:47 -0300

    Input & Button components

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input'
+import Button from '~/components/Button'
 import Background from '~/components/Background'
 
 // import { Container } from './styles';
@@ -8,7 +10,13 @@ import Background from '~/components/Background'
 const SignIn = () => {
   return (
     <Background>
-      <Text>SignIn apenas</Text>
+      <Text>SignIn qualquer</Text>
+      <Input
+        style={{marginTop: 30}}
+        icon='call'
+        placeholder='Digite seu número'
+      />
+      <Button>Entrar</Button>
     </Background>
   )
 }
