0b928e6ac34fdfbabe900eb364d7b06b583ddc35
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-06-01 12:03:35 -0300

    background configured

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Background from '~/components/Background'
+
 // import { Container } from './styles';
 
 const SignIn = () => {
-  return <Text>SignIn Route</Text>;
+  return (
+    <Background>
+      <Text>SignIn apenas</Text>
+    </Background>
+  )
 }
 
 export default SignIn;
