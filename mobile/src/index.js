3bf4452f4e822fbaebb6300246bfc7ff9154c9c9
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-06-02 12:06:12 -0300

    Statusbar config

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,11 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'react-native'
 import './config/ReactotronConfig'
 
 import Routes from './routes'
 
 const src = () => {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle='light-content' backgroundColor='#7159c1' />
+      <Routes />
+    </>
+  )
 }
 
 export default src;
