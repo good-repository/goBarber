490d65a45b7d52ce48280570ac704928ed757d15
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-06-03 18:24:58 -0300

    loading and canceling appointments

--- mobile/src/components/Appointment/index.js
+++ mobile/src/components/Appointment/index.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns'
+import pt from 'date-fns/locale/pt'
 import { TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-const Appointment = () => {
+const Appointment = ({ data, onCancel }) => {
+    const dateParsed = useMemo(
+        () => {
+            return formatRelative(parseISO(data.date), new Date(), {
+                locale: pt,
+                addSufix: true
+            })
+        },
+        [data.date],
+    )
+
     return (
-        <Container>
+        <Container past={data.past}>
             <Left>
-                <Avatar source={{ uri: 'https://api.adorable.io/avatars/50/abott@adorable.png' }} />
+                <Avatar source={{
+                    uri: data.provider.avatar ? data.provider.avatar.url :
+                        `https://api.adorable.io/avatars/50/${data.provider.name}.png`
+                }}
+                />
 
                 <Info>
-                    <Name>Guilherme Fünkler</Name>
-                    <Time>em 3 horas</Time>
+                    <Name>{data.provider.name}</Name>
+                    <Time>{dateParsed}</Time>
                 </Info>
             </Left>
 
-            <TouchableOpacity osPress={() => { }}>
-                <Icon name='event-busy' size={20} color='#f64c75' />
-            </TouchableOpacity>
+            {data.cancelable && !data.canceled_at && (
+                <TouchableOpacity onPress={onCancel}>
+                    <Icon name='event-busy' size={20} color='#f64c75' />
+                </TouchableOpacity>
+            )}
         </Container>
     )
 }
