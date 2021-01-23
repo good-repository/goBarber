40d3aa27bd18f4b005ec89490d75025d2fd7aecc
Guilheme Fünkler Borelli <borelli.guilherme@gmail.com>
2021-01-23 17:50:34 -0300

    Update saga.js

--- mobile/src/store/modules/auth/saga.js
+++ mobile/src/store/modules/auth/saga.js
@@ -1,67 +1,73 @@
-import { Alert } from 'react-native'
-import { takeLatest, call, put, all } from 'redux-saga/effects'
+import {Alert} from 'react-native';
+import {takeLatest, call, put, all} from 'redux-saga/effects';
+import * as RootNavigation from '../../../config/RootNavigation';
 
-import api from '../../../services/api'
+import api from '../../../services/api';
 
-import { signInSuccess, signFailure } from './actions'
+import {signInSuccess, signFailure} from './actions';
 
-export function* signIn({ payload }) {
-    try {
-        const { email, password } = payload
+export function* signIn({payload}) {
+  try {
+    const {email, password} = payload;
 
-        const response = yield call(api.post, 'sessions', {
-            email,
-            password
-        })
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-        const { token, user } = response.data
+    const {token, user} = response.data;
 
-        if (user.provider) {
-            Alert.alert('Erro no login', 'Usuário não pode ser prestador de serviços')
-            return
-        }
-
-        api.defaults.headers.Authorization = `Bearer ${token}`
-
-        yield put(signInSuccess(token, user))
-
-        // history.push('/dashboard')
-    } catch (err) {
-        Alert.alert('Falha na autenticação', 'Erro no login, verifique seus dados')
-        yield put(signFailure())
+    if (user.provider) {
+      Alert.alert(
+        'Erro no login',
+        'Usuário não pode ser prestador de serviços',
+      );
+      return;
     }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    // history.push('/dashboard')
+  } catch (err) {
+    Alert.alert('Falha na autenticação', 'Erro no login, verifique seus dados');
+    yield put(signFailure());
+  }
 }
 
-export function* signUp({ payload }) {
-    try {
-        const { name, email, password } = payload
+export function* signUp({payload}) {
+  try {
+    const {name, email, password} = payload;
 
-        yield call(api.post, 'users', {
-            name,
-            email,
-            password
-        })
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+    });
 
-        // history.push('/')
-    } catch (err) {
-        Alert.alert('Falha no cadastro', 'Erro no cadastro, verifique seus dados e tente novamente')
-        yield put(signFailure())
-    }
+    yield RootNavigation.navigate('SignIn');
+  } catch (err) {
+    Alert.alert(
+      'Falha no cadastro',
+      'Erro no cadastro, verifique seus dados e tente novamente',
+    );
+    yield put(signFailure());
+  }
 }
 
-export function setToken({ payload }) {
-    if (!payload) return
+export function setToken({payload}) {
+  if (!payload) return;
 
-    const { token } = payload.auth
+  const {token} = payload.auth;
 
-    if (token) {
-        api.defaults.headers.Authorization = `Bearer ${token}`
-    }
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
 }
-
 
 export default all([
-    takeLatest('persist/REHYDRATE', setToken),
-    takeLatest('@auth/SIGN_IN_REQUEST', signIn),
-    takeLatest('@auth/SIGN_UP_REQUEST', signUp),
-])
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
