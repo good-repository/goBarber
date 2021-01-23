7b53b7f5c8fdd26ced2140da99de308c96c7c116
Guilheme Fünkler Borelli <borelli.guilherme@gmail.com>
2021-01-23 17:50:14 -0300

    Update index.js

--- mobile/src/pages/New/SelectProvider/index.js
+++ mobile/src/pages/New/SelectProvider/index.js
@@ -1,43 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
-import api from '../../../services/api'
+import api from '../../../services/api';
 
-import Background from '../../../components/Background'
+import Background from '../../../components/Background';
 
-import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+import {Container, ProvidersList, Provider, Avatar, Name} from './styles';
 
-const SelectProvider = ({ navigation }) => {
-    const [providers, setProviders] = useState([])
+const SelectProvider = ({navigation}) => {
+  const [providers, setProviders] = useState([]);
 
-    useEffect(() => {
-        async function loadProviders() {
-            const response = await api.get('providers')
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
 
-            setProviders(response.data)
-        }
+      setProviders(response.data);
+    }
 
-        loadProviders()
-    }, [])
+    loadProviders();
+  }, []);
 
-    return (
-        <Background>
-            <Container>
-                <ProvidersList
-                    data={providers}
-                    keyExtractor={provider => String(provider.id)}
-                    renderItem={({ item: provider }) => (
-                        <Provider onPress={() => navigation.navigate('SelectDateTime', { provider })} >
-                            <Avatar source={{
-                                uri: provider.avatar ? provider.avatar.url :
-                                    `https://api.adorable.io/avatars/50/${provider.name}.png`
-                            }} />
-                            <Name>{provider.name}</Name>
-                        </Provider>
-                    )}
-                />
-            </Container>
-        </Background>
-    )
-}
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({item: provider}) => (
+            <Provider
+              onPress={() => navigation.navigate('SelectDateTime', {provider})}>
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://avatars.dicebear.com/4.5/api/avataaars/${
+                        provider.name
+                      }.svg?w=50&h=50`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
+};
 
 export default SelectProvider;
