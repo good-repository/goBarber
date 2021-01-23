d1d9346c3a9093539b5d7265d1ae8b038663a304
Guilheme Fünkler Borelli <borelli.guilherme@gmail.com>
2021-01-23 17:49:34 -0300

    Update index.js

--- mobile/src/components/Appointment/index.js
+++ mobile/src/components/Appointment/index.js
@@ -1,44 +1,45 @@
-import React, { useMemo } from 'react';
-import { parseISO, formatRelative } from 'date-fns'
-import pt from 'date-fns/locale/pt'
-import { TouchableOpacity } from 'react-native'
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import React, {useMemo} from 'react';
+import {parseISO, formatRelative} from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import {TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Left, Avatar, Info, Name, Time } from './styles';
+import {Container, Left, Avatar, Info, Name, Time} from './styles';
 
-const Appointment = ({ data, onCancel }) => {
-    const dateParsed = useMemo(
-        () => {
-            return formatRelative(parseISO(data.date), new Date(), {
-                locale: pt,
-                addSufix: true
-            })
-        },
-        [data.date],
-    )
+const Appointment = ({data, onCancel}) => {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSufix: true,
+    });
+  }, [data.date]);
 
-    return (
-        <Container past={data.past}>
-            <Left>
-                <Avatar source={{
-                    uri: data.provider.avatar ? data.provider.avatar.url :
-                        `https://api.adorable.io/avatars/50/${data.provider.name}.png`
-                }}
-                />
+  return (
+    <Container past={data.past}>
+      <Left>
+        <Avatar
+          source={{
+            uri: data.provider.avatar
+              ? data.provider.avatar.url
+              : `https://avatars.dicebear.com/4.5/api/avataaars/${
+                  data.provider.name
+                }.svg?w=50&h=50`,
+          }}
+        />
 
-                <Info>
-                    <Name>{data.provider.name}</Name>
-                    <Time>{dateParsed}</Time>
-                </Info>
-            </Left>
+        <Info>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
+        </Info>
+      </Left>
 
-            {data.cancelable && !data.canceled_at && (
-                <TouchableOpacity onPress={onCancel}>
-                    <Icon name='event-busy' size={20} color='#f64c75' />
-                </TouchableOpacity>
-            )}
-        </Container>
-    )
-}
+      {data.cancelable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
+    </Container>
+  );
+};
 
 export default Appointment;
