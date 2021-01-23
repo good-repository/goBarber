3e42a7a31db7bf461385be944416f2fb650f7ac7
Guilheme Fünkler Borelli <borelli.guilherme@gmail.com>
2021-01-23 17:49:51 -0300

    Update index.js

--- mobile/src/pages/New/Confirm/index.js
+++ mobile/src/pages/New/Confirm/index.js
@@ -1,49 +1,55 @@
-import React, { useMemo } from 'react';
-import { StackActions } from '@react-navigation/native'
-import { formatRelative, parseISO } from 'date-fns'
-import pt from 'date-fns/locale/pt'
+import React, {useMemo} from 'react';
+import {StackActions} from '@react-navigation/native';
+import {formatRelative, parseISO} from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-import api from '../../../services/api'
+import api from '../../../services/api';
 
-import Background from '../../../components/Background'
-import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+import Background from '../../../components/Background';
+import {Container, Avatar, Name, Time, SubmitButton} from './styles';
 
-const Confirm = ({ navigation, route }) => {
-  const provider = route.params.provider
-  const time = route.params.time
+const Confirm = ({navigation, route}) => {
+  const provider = route.params.provider;
+  const time = route.params.time;
 
   const dateFormatted = useMemo(
-    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
-    [time]
-  )
+    () => formatRelative(parseISO(time), new Date(), {locale: pt}),
+    [time],
+  );
 
   async function handleAddAppointment() {
     await api.post('appointments', {
       provider_id: provider.id,
-      date: time
-    })
+      date: time,
+    });
 
     navigation.dispatch(StackActions.popToTop());
-    navigation.navigate('Dashboard')
+    navigation.navigate('Dashboard');
   }
 
   return (
     <Background>
       <Container>
-        <Avatar source={{
-          uri: provider.avatar ? provider.avatar.url :
-            `https://api.adorable.io/avatars/50/${provider.name}.png`
-        }} />
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url
+              : `https://avatars.dicebear.com/4.5/api/avataaars/${
+                  provider.name
+                }.svg?w=50&h=50`,
+          }}
+        />
 
         <Name>{provider.name}</Name>
 
         <Time>{dateFormatted}</Time>
 
-        <SubmitButton onPress={() => handleAddAppointment()}>Confirmar Agendamento</SubmitButton>
-
+        <SubmitButton onPress={() => handleAddAppointment()}>
+          Confirmar Agendamento
+        </SubmitButton>
       </Container>
     </Background>
   );
-}
+};
 
 export default Confirm;
